d3e1ae67fabc4c1efe5d1f972c6542a26d9468ab
jetrosa <jetro.saarti@gmail.com>
2023-01-30 14:46:38 +0200

    Trainer page component init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TrainerComponent } from './pages/trainer/trainer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainerComponent
   ],
   imports: [
     BrowserModule
