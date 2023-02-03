0a13042615eb3f0ef2f1f002e09cd7703262da36
Milovane <122837001+Milovane@users.noreply.github.com>
2023-02-03 15:00:51 +0100

    Changed favorite button

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUsers: 'https://radical-clover-sunscreen.glitch.me/trainers',
-  apiPokemons: 'https://pokeapi.co/api/v2/pokemon',
+  apiPokemons: 'https://pokeapi.co/api/v2/pokemon?limit=905&offset=0',
   apiKey: "x(l`7rkRq|!e}vJcWB`tCGVviez-[L;TD?'l))LkvY{}S7r~=SodR0H.U{)8}.yU",
   pokemonImgBaseUrl:
     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon',
